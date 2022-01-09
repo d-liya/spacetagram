5cc69bfde8b27c420eaf72d4eb4f2633c39419c2
Dilum <dilum14@gmail.com>
2022-01-09 06:12:16 -0330

    changed the modal color and handeled oveflow issue

--- src/components/ReadMore.tsx
+++ src/components/ReadMore.tsx
@@ -18,30 +18,33 @@ export default function ReadMore() {
   });
 
   return data ? (
-    <animated.div className="w-full min-h-screen z-100 top-0 fixed bg-dark-green">
-      <animated.div className="flex justify-center items-center min-h-screen overflow-auto">
+    <animated.div className="w-full min-h-screen z-100 top-0 fixed bg-dark-green ">
+      <animated.div className="flex justify-center items-center min-h-screen ">
         <div
-          className="w-4/5 bg-dark-green shadow-2xl shadow-light-green rounded flex flex-col items-center justify-center  p-10 relative"
-          style={{ minHeight: "90vh" }}
+          className="w-4/5 bg-light-cream shadow-2xl shadow-light-green rounded flex flex-col items-center justify-center  md:p-10 relative"
+          style={{ height: "90vh" }}
         >
           <Link
             to="/"
-            className="absolute top-10 right-10 text-green-100 bg-light-green rounded-full p-2 hover:scale-110 hover:opacity-50 transition ease-in-out duration-300"
+            className="absolute top-10 right-10 text-green-100 bg-light-green rounded-full shadow-md shadow-light-green p-2 hover:scale-110 hover:opacity-50 transition ease-in-out duration-300"
           >
             <AiOutlineClose size={18} />
           </Link>
-          <animated.h1 style={style} className="text-green-100 font-bold">
+          <animated.h1 style={style} className="text-light-brown p-5 font-bold">
             {data.title}
           </animated.h1>
           {!!data.copyright && (
-            <animated.h3 style={style} className="text-sm text-green-100 ">
+            <animated.h3 style={style} className="text-sm text-dark-green ">
               By {data.copyright}
             </animated.h3>
           )}
-          <animated.time style={style} className="text-sm text-green-100 pb-2">
+          <animated.time style={style} className="text-sm text-dark-green pb-2">
             {moment(data.date).format("MMMM Do YYYY")}
           </animated.time>
-          <animated.p style={style} className="p-2 text-green-100 text-center">
+          <animated.p
+            style={style}
+            className="py-2 px-5 overflow-scroll text-slate-900 text-center"
+          >
             {data.explanation}
           </animated.p>
         </div>
